60b0b9cbd76927e3cb98d3fbf3acff9a6344c227
pcerjakTSZ <patrikcerjak03@gmail.com>
2025-09-23 08:57:44 +0200

    Update and rename zadaci.js to 23.9.dz.js

---
+++ 23.9.dz.js
@@ -0,0 +1,39 @@
+-- 1
+
+FOR (i = 0 ; i < l.niz ; i ++)
+    FOR (j= i + 1 ; j < l.niz ; j++)
+        IF (niz [i] > niz [j
+
+
+
+
+-- 2
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+DZ
+napisati funcije za enkripciju i dekripciju 
+po slj. pravilima: 
+                samoglasnik > 3 mjesta
+                suglasnik > 5
+                svi ostali znakovi > 7
+                razmak se ne mijenja
+
