34c65c971c71324bc879232f00ae3deb87477131
pcerjakTSZ <patrikcerjak03@gmail.com>
2025-09-30 08:00:35 +0200

    Update 30.9.dz.js

--- 23.9.dz.js
+++ 23.9.dz.js
@@ -53,9 +53,7 @@ ostalo - pomice za 7
 const samoglasnik = [97, 101, 105, 111, 117];
 const suglasnik = [98, 99, 100, 102, 103, 104, 106, 107, 108, 109, 110, 112, 113, 114, 115, 116, 118, 119, 120, 121, 122];
 
-
-let string = "the quick red fox jumped over the lazy dog 1234567890 !#$%&/()=";
-
+let string = "macka skace preko ograde 1234567890 !#$%&/()=?*";
 
 function encrypt(string){
     let encrypted = {
@@ -66,18 +64,16 @@ function encrypt(string){
     for (let i = 0; i < string.length; i++){
         let fromAscii = string[i].charCodeAt();
 
-        // ako je samoglasnik
         if (samoglasnik.includes(fromAscii)) {
             encrypted.main += String.fromCharCode(fromAscii + 3);
             encrypted.location.push(i);
         }
-
+        
         // suglasnik
         else if (suglasnik.includes(fromAscii)){
             encrypted.main += String.fromCharCode(fromAscii + 5);
         }
 
-        // ostalo (brojevi, znakovi)
         else {
             encrypted.main += String.fromCharCode(fromAscii + 7);
         }
@@ -86,7 +82,6 @@ function encrypt(string){
 
     return encrypted
 }
-
 
 function decrypt(encrypted){
     let decrypted = "";
@@ -101,22 +96,18 @@ function decrypt(encrypted){
             continue;
         }
 
-        // suglasnik
         else if (suglasnik.includes(fromAscii - 5)){
             decrypted += String.fromCharCode(fromAscii - 5);
         }
 
-        // ostalo (brojevi, znakovi)
         else {
             decrypted += String.fromCharCode(fromAscii - 7);
         }
-
 
     }
 
     return decrypted
 }
-
 
 let x = encrypt(string);
 let y = decrypt(x);
